269a3e59d5c6e335080d11464ba30d3fadc6b718
GANESH GOPU <ganeshreddygopu@gmail.com>
2026-02-23 12:17:17 -0500

    Add isPalindrome function skeleton

---
+++ isPalindrome.js
@@ -0,0 +1,11 @@
+/**
+ * Check if a string is a palindrome
+ * - ignore spaces, punctuation, and case
+ * @param {string} input
+ * @returns {boolean}
+ */
+function isPalindrome(input) {
+  // TODO: implement
+}
+
+module.exports = isPalindrome;
